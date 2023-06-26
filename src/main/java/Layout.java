e4909355998f3b86abbb18dce5128a5cda0ad16a
Quincy Williams <QLWilliams@student.fullsail.edu>
2023-06-26 12:27:26 -0400

    Revert "Merge branch 'FileDestinationV1' into TestBranch"
    This reverts commit cd88b2356cb35596090becf691d7babf6a2d7111, reversing
    changes made to 3637dc1cbbd91266590bb74ff61d96bf2bcef876.

--- src/main/java/Layout.java
+++ src/main/java/Layout.java
@@ -1,15 +1,9 @@
 import javax.swing.*;
-import javax.swing.event.MenuEvent;
-import javax.swing.event.MenuListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.File;
-import java.io.IOException;
-
-import static javax.swing.JOptionPane.showMessageDialog;
 
 public class Layout {
 
@@ -24,17 +18,12 @@ public class Layout {
         frame.setSize(450, 275);
         //frame.setLayout(new BorderLayout(20,15));
 
-        //Create the components of the menu
+        //Create the components of the frame
         JMenuBar menubar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
-        JMenu aboutMenu = new JMenu("About");
         menubar.add(fileMenu);
-        menubar.add(aboutMenu);
-
-        //Add options to menu
-        JMenuItem fileLocationMB = new JMenuItem("File Location");
-        JMenuItem openFile = new JMenuItem("Open File");
-        fileMenu.add(fileLocationMB);
+        JMenuItem fileItem1 = new JMenuItem("File Location");
+        fileMenu.add(fileItem1);
 
         //Create category dropdown
         JComboBox<String> categories = new JComboBox<>(category);
@@ -78,52 +67,6 @@ public class Layout {
                 } else {
                     responseArea.setText(query.getResponse());
                     searchArea.requestFocus();
-                }
-            }
-        });
-
-        //Add functionality for setting the file location
-        fileLocationMB.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FileDestination fd = new FileDestination();
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-                int fileLocation = fileChooser.showOpenDialog(frame);
-
-                if (fileLocation == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile();
-                    fd.setFileDestination(file.toString());
-
-                    showMessageDialog(null, "File changed!");
-
-                    searchArea.setText("");
-                    responseArea.setText("iparam details will display here");
-                    searchArea.requestFocus();
-                }
-            }
-        });
-
-        //Add functionality for opening the file
-        openFile.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                FileDestination fd = new FileDestination();
-                File file = new File(fd.getFileDestination());
-
-                //first check if Desktop is supported by Platform or not
-                if(!Desktop.isDesktopSupported()){
-                    System.out.println("Desktop is not supported");
-                    return;
-                }
-
-                Desktop desktop = Desktop.getDesktop();
-                if(file.exists()) {
-                    try {
-                        desktop.open(file);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
                 }
             }
         });
--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -13,6 +13,7 @@ import static javax.swing.JOptionPane.showMessageDialog;
 public class Search {
 
     private String response;
+    private String fileDestination = "./src/main/java/iparamListV2.xlsx"; //Hardcoded for testing only
 
     public void search(String iparam) {
         if(iparam.equals("")) {
@@ -20,29 +21,61 @@ public class Search {
             return;
         }
 
-        FileDestination fd = new FileDestination();
+        try (FileInputStream fileInputStream = new FileInputStream(getFileDestination())){
 
-        try (FileInputStream fileInputStream = new FileInputStream(fd.getFileDestination())){
+            String category = "";
+            String example = "";
 
-            if (fd.getFileDestination().endsWith(".xlsx")) {
+            if (getFileDestination().endsWith(".xlsx")) {
                 XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
                 XSSFSheet sheet = workbook.getSheetAt(0);
 
+
                 for (Row row : sheet) {
-                    if (row.getCell(0).getStringCellValue().equals(iparam)) {
-                        setResponse(row.getCell(1).getStringCellValue());
+                    if (row.getCell(0).getStringCellValue().equalsIgnoreCase(iparam)) {
+                        if(row.getCell(3).getStringCellValue().equals("Multiple Categories")) {
+                            category = "General";
+                        } else {
+                            category = row.getCell(3).getStringCellValue();
+                        }
+
+                        if(row.getCell(2).getStringCellValue().equals("")) {
+                            example = "No value required or custom param";
+                        } else {
+                            example = row.getCell(2).getStringCellValue();
+                        }
+
+                        setResponse(row.getCell(1).getStringCellValue() + '\n' +  '\n' + "Example"
+                         +'\n' + row.getCell(0).getStringCellValue() + "               " +
+                                example + '\n' + '\n' + "Category: " +
+                                category);
                         break;
                     } else {
                         setResponse("iparam not found");
                     }
                 }
-            } else if (fd.getFileDestination().endsWith("xls")) {
+            } else if (getFileDestination().endsWith("xls")) {
                 HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
                 HSSFSheet sheet = workbook.getSheetAt(0);
 
                 for (Row row : sheet) {
-                    if (row.getCell(0).getStringCellValue().equals(iparam)) {
-                        setResponse(row.getCell(1).getStringCellValue());
+                    if (row.getCell(0).getStringCellValue().equalsIgnoreCase(iparam)) {
+                        if(row.getCell(3).getStringCellValue().equals("Multiple Categories")) {
+                            category = "General";
+                        } else {
+                            category = row.getCell(3).getStringCellValue();
+                        }
+
+                        if(row.getCell(2).getStringCellValue().equals("")) {
+                            example = "No value required or custom param";
+                        } else {
+                            example = row.getCell(2).getStringCellValue();
+                        }
+
+                        setResponse(row.getCell(1).getStringCellValue() + '\n' +  '\n' + "Example"
+                                +'\n' + row.getCell(0).getStringCellValue() + "               " +
+                                example + '\n' + '\n' + "Category: " +
+                                category);
                         break;
                     } else {
                         setResponse("iparam not found");
@@ -52,7 +85,8 @@ public class Search {
 
         }
         catch (Exception e) {
-            showMessageDialog(null, "Cannot read file or file type is incorrect. \nPlease check file location and confirm" +
+            showMessageDialog(null, "Cannot read file or file type is incorrect. \nPlease check " +
+                    "file location and confirm" +
                     " that the extension is either .xlsx or .xls");
             setResponse("iparam not found");
         }
@@ -67,4 +101,11 @@ public class Search {
         return response;
     }
 
+    public void setFileDestination(String FD) {
+        this.fileDestination = FD;
+    }
+
+    public String getFileDestination() {
+        return fileDestination;
+    }
 }
